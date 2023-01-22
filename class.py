1957800814f85921cb0ffad6af0da9c3e1d8a0a7
Seoyeon Kim <syk991116@korea.ac.kr>
2023-01-22 09:01:36 +0900

    update: 23.01.22

--- class.py
+++ class.py
@@ -139,3 +139,130 @@ class FlyableAttackUnit(AttackUnit, Flyable):
 valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
 valkyrie.fly(valkyrie.name, "3시")
 
+# 메소드 오버라이딩
+class Unit:
+    def __init__(self, name, hp, speed):
+        self.name = name
+        self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]" .format(self.name, location, self.speed))
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]" .format(self.name, location, self.damage))
+           
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다." .format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다." .format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다." .format(self.name))
+ 
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]" .format(name, location, self.flying_speed))
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 스피드는 0
+        Flyable.__init__(self, flying_speed)
+
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+battlecruiser.fly(battlecruiser.name, "9시")
+# 똑같이 move함수를 써서 지상유닛은 이동하고, 공중유닛은 날 수 있도록 하려면..!
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 스피드는 0
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+vulture.move("11시")
+battlecruiser.move("9시")
+
+# pass
+class BulidingUnit(Unit):
+    def __init__(self, name, hp, location):
+        pass # 아무것도 안하고 일단 넘어간다.
+
+supply_depot = BulidingUnit("서플라이 디폿", 500, "7시")
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    pass
+
+game_start()
+game_over()
+
+# super - 부모 클래스로 바로 사용.
+class BulidingUnit(Unit):
+    def __init__(self, name, hp, location):
+        #Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0) # self를 쓰지 않는다.
+        self.location = location
+
+## but 다중상속할 경우 문제가 발생.
+
+class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+class Flyable:
+    def __init__(self):
+        print("Flyable 생성자")
+
+class FlyableUnit(Unit, Flyable):
+    def __init__(self):
+        super().__init__() 
+
+dropship = FlyableUnit() # Unit 클래스만 사용됨. Flyable은 안쓰임.
+
+class FlyableUnit(Flyable, Unit):
+    def __init__(self):
+        super().__init__() 
+
+dropship = FlyableUnit() # 두개 이상 다중상속 시 super() -> 앞의 부모 클래스의 것을 상속받는다.
+
+
+## 퀴즈
+
+class House:
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
+
+    def show_detail(self):
+        print("{0} {1} {2} {3} {4}년" .format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
+
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007")
+house3 = House("송파", "빌라", "월세", "500/50", "2000")
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print("총 {0}대의 매물이 있습니다." .format(len(houses)))
+for house in houses:
+    house.show_detail()
